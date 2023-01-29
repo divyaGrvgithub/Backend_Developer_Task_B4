0858517776309898676819b16b1c65ee1875c45c
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-01-29 19:56:21 +0530

    done

---
+++ src/models/adminModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const adminModel=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:"Employee"
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model("admin",adminModel)
---
+++ src/models/courseModel.js
@@ -0,0 +1,39 @@
+const mongoose  = require("mongoose")
+const objectId=mongoose.Schema.Types.ObjectId
+const courseSchema=mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    videoUrl:{
+        type:String,
+        required:true
+    },
+    topicsArray:{
+        type:Array,
+        required:true
+    },
+    duration:{
+        type:String,
+        raquired:true
+    },
+    category:{
+        type:String,
+        raquired:true
+    },
+    adminId:{
+        type:objectId,
+        ref:"employee"
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    isAprovved:Boolean
+},{timestamps:true})
+
+module.exports=mongoose.model("course",courseSchema)
